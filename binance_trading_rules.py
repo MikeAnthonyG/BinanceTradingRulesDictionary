b5a467b3b7efcf15e2911bbc1dc1c75bec39321c
Mike G <mgrutsch@gmail.com>
2017-10-25 18:22:41 -0500

    Trading Rules
    A dictionary for Binance Trading Rules, helpful to use with the API.

---
+++ binance_trading_rules.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 20:20:37 2017
+
+@author: Mikey
+"""
+
+trading_rules = {}
+
+#BTC
+trading_rules['ETHBTC'] = {'pair':'ETHBTC', 'min_amount':0.001, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['LTCBTC'] = {'pair':'LTCBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['BNBBTC'] = {'pair':'BNBBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['NEOBTC'] = {'pair':'NEOBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['GASBTC'] = {'pair':'GASBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['BCCBTC'] = {'pair':'BCCBTC', 'min_amount':0.001, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['MCOBTC'] = {'pair':'MCOBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['WTCBTC'] = {'pair':'WTCBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['QTUMBTC'] = {'pair':'QTUMBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['OMGBTC'] = {'pair':'OMGBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['ZRXBTC'] = {'pair':'ZRXBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['STRATBTC'] = {'pair':'STRATBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['SNGLSBTC'] = {'pair':'SNGLSBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['BQXBTC'] = {'pair':'BQXBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['KNCBTC'] = {'pair':'KNCBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['FUNBTC'] = {'pair':'FUNBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['SNMBTC'] = {'pair':'SNMBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['LINKBTC'] = {'pair':'LINKBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['XVGBTC'] = {'pair':'XVGBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['CTRBTC'] = {'pair':'CTRBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['SALTBTC'] = {'pair':'SALTBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['IOTABTC'] = {'pair':'IOTABTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['MDABTC'] = {'pair':'MDABTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['MTLBTC'] = {'pair':'MTLBTC', 'min_amount':1.0, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['SUBBTC'] = {'pair':'SUBBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['EOSBTC'] = {'pair':'EOSBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['SNTBTC'] = {'pair':'SNTBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['ETCBTC'] = {'pair':'ETCBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['MTHBTC'] = {'pair':'MTHBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['ENGBTC'] = {'pair':'ENGBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['DNTBTC'] = {'pair':'DNTBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['BNTBTC'] = {'pair':'BNTBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['ASTBTC'] = {'pair':'ASTBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['DASHBTC'] = {'pair':'DASHBTC', 'min_amount':0.001, 'min_price':0.000001, 'min_order_value': 0.001}
+trading_rules['ICNBTC'] = {'pair':'ICNBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['OAXBTC'] = {'pair':'OAXBTC', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.001}
+trading_rules['BTGBTC'] = {'pair':'BTGBTC', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.001}
+
+#ETH
+trading_rules['BNBETH'] = {'pair':'BNBETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['QTUMETH'] = {'pair':'QTUMETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['SNTETH'] = {'pair':'SNTETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['BNTETH'] = {'pair':'BNTETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['EOSETH'] = {'pair':'EOSETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['OAXETH'] = {'pair':'OAXETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['DNTETH'] = {'pair':'DNTETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['MCOETH'] = {'pair':'MCOETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['ICNETH'] = {'pair':'ICNETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['WTCETH'] = {'pair':'WTCETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['OMGETH'] = {'pair':'OMGETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['ZRXETH'] = {'pair':'ZRXETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['STRATETH'] = {'pair':'STRATETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['SNGLSETH'] = {'pair':'SNGLSETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['BQXETH'] = {'pair':'BQXETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['KNCETH'] = {'pair':'KNCETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['FUNETH'] = {'pair':'FUNETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['SNMETH'] = {'pair':'SNMETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['NEOETH'] = {'pair':'NEOETH', 'min_amount':0.01, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['LINKETH'] = {'pair':'LINKETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['XVGETH'] = {'pair':'XVGETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['CTRETH'] = {'pair':'CTRETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['SALTETH'] = {'pair':'SALTETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['IOTAETH'] = {'pair':'IOTAETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['MDAETH'] = {'pair':'MDAETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['MTLETH'] = {'pair':'MTLETH', 'min_amount':1.0, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['SUBETH'] = {'pair':'SUBETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['ETCETH'] = {'pair':'ETCETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+trading_rules['MTHETH'] = {'pair':'MTHETH', 'min_amount':1.0, 'min_price':0.00000001, 'min_order_value': 0.01}
+trading_rules['ENGETH'] = {'pair':'ENGETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['ASTETH'] = {'pair':'ASTETH', 'min_amount':1.0, 'min_price':0.0000001, 'min_order_value': 0.01}
+trading_rules['DASHETH'] = {'pair':'DASHETH', 'min_amount':0.001, 'min_price':0.00001, 'min_order_value': 0.01}
+trading_rules['BTGETH'] = {'pair':'BTGETH', 'min_amount':0.01, 'min_price':0.000001, 'min_order_value': 0.01}
+
+#USDT
+trading_rules['BTCUSDT'] = {'pair':'BTCUSDT', 'min_amount':0.000001, 'min_price': 0.01, 'min_order_value': 1}
+trading_rules['ETHUSDT'] = {'pair':'ETHUSDT', 'min_amount':0.00001, 'min_price': 0.01, 'min_order_value': 1}
